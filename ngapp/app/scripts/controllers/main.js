f4358cea9dbdbf602ec962b2575d212553cb21c5
tomohiro1221 <tomo.light.1221@me.com>
2014-06-25 19:01:33 +0900

    Updates ngapp

--- ngapp/app/scripts/controllers/main.js
+++ ngapp/app/scripts/controllers/main.js
@@ -1,10 +1,50 @@
 'use strict';
 
 angular.module('ngappApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .factory('todos', function($http) {
+    var format_params = function (data) {
+      return { todo : data }
+    };
+    return {
+      get: function() {
+        return $http.get('/api/todos.json');
+      },
+      create: function(todoData) {
+        return $http.post('/api/todos.json', format_params(todoData));
+      },
+      delete: function(id) {
+        return $http.delete('/api/todos/' + id + '.json');
+      }
+    }
+  })
+  .directive('superman', function () {
+    return {
+      restrict: "A",
+      link: function () {
+        alert('I\'m working')
+      }
+    }
+  })
+  .controller('MainCtrl', function ($scope, $http, todos) {
+    todos.get()
+      .success(function(data) {
+        $scope.todos = data;
+      });
+
+    $scope.createTodo = function () {
+      if (!$.isEmptyObject($scope.formData)) {
+        todos.create($scope.formData)
+          .success(function (data) {
+            $scope.formData = {};
+            $scope.todos.push(data);
+          });
+      }
+    };
+
+    $scope.deleteTodo = function (item) {
+      todos.delete(item.id)
+        .success(function () {
+          $scope.todos.splice($scope.todos.indexOf(item)); 
+        })
+    };
   });
